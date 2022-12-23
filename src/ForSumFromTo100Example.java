7099bb285b9163391d1df82ed22bbabf6ada1320
Changhogod <younch8342@naver.com>
2022-12-23 17:11:10 +0900

    1부터 100까지 for문

---
+++ src/ForSumFromTo100Example.java
@@ -0,0 +1,10 @@
+public class ForSumFromTo100Example {
+    public static void main(String[] args) {
+        int sum =0;
+
+        for(int i=1; i<=100; i++){
+            sum=sum+i;
+        }
+        System.out.println("1부터 100까지의 합은 : "+sum +"입니다.");
+    }
+}
