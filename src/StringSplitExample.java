91e72a59bf7995a41d270d1120964b92ce19362b
Changhogod <younch8342@naver.com>
2022-12-24 22:33:30 +0900

    StringTokenizer&split

---
+++ src/StringSplitExample.java
@@ -0,0 +1,17 @@
+import java.util.StringTokenizer;
+
+public class StringSplitExample {
+    public static void main(String[] args) {
+        String text= "홍길동&이수홍,박연수,김자바-최명호";
+        String[] names =text.split("&|-|,");
+        for(String name :names){
+            System.out.println("name = " + name);
+        }
+        String text2 = "홍길동/이수홍/박연수";
+        StringTokenizer st = new StringTokenizer(text2,"/");
+        while(st.hasMoreTokens()) {
+            String name = st.nextToken();
+            System.out.println("name = " + name);
+        }
+    }
+}
