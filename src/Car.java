a1cc5f70f25e8cca21a2812ebd7b67f08e41bfc4
Changhogod <younch8342@naver.com>
2022-12-24 17:18:26 +0900

    add class field

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,13 @@
 public class Car {
     String company = "현대자동차";
     String model = "그랜저";
-    String color = "검정";
+    String color;
     int maxSpeed = 350;
     int speed;
+
+    int CC;
+    Car(String color, int cc){
+        this.color =color;
+        this.CC =cc;
+    }
 }
--- src/CarExample.java
+++ src/CarExample.java
@@ -1,6 +1,6 @@
 public class CarExample {
     public static void main(String[] args) {
-        Car myCar = new Car();
+        Car myCar = new Car("파랑",3000);
 
         System.out.println("제작회사 : "+myCar.company);
         System.out.println("모델명 : "+myCar.model);
@@ -10,5 +10,6 @@ public class CarExample {
 
         int curSpeed =myCar.speed = 50;
         System.out.println("내차의 현재 속도는 "+curSpeed+"km 입니다.");
+        System.out.println("내차의 cc는 "+myCar.CC+"CC 입니다.");
     }
 }
