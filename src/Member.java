afffbc785299359c7a48b73a00ea730ab7ce94ef
Changhogod <younch8342@naver.com>
2022-12-25 00:26:50 +0900

    sorting & compareTo

---
+++ src/Member.java
@@ -0,0 +1,12 @@
+public class Member implements Comparable<Member>{
+    String name;
+    Member(String name){
+        this.name = name;
+    }
+    @Override
+    public int compareTo(Member o){
+        return name.compareTo(o.name);
+    }
+
+
+}
