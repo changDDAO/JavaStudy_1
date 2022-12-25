ddce10a80e5cd3174c084516a6aacd2bf0f77f9a
Changhogod <younch8342@naver.com>
2022-12-25 17:57:24 +0900

    manage shared Object

---
+++ src/JavaStudy_2/Thread/User1.java
@@ -0,0 +1,13 @@
+package JavaStudy_2.Thread;
+
+public class User1 extends Thread{
+    private Calculator calculator;
+
+    public void setCalculator(Calculator calculator){
+        this.setName("User1");
+        this.calculator = calculator;
+    }
+    public void run(){
+        calculator.setMemory(100);
+    }
+}
