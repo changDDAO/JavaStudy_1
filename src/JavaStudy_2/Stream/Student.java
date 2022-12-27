fce96690af26291abe2a4fced07a158d4f1e803c
Changhogod <younch8342@naver.com>
2022-12-28 01:31:35 +0900

    Student & Member class

---
+++ src/JavaStudy_2/Stream/Student.java
@@ -0,0 +1,37 @@
+package JavaStudy_2.Stream;
+
+public class Student implements Comparable<Student>{
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if(this.score<o.score)
+        return -1;
+        else if(this.score==o.score)
+                return 0;
+        else return 1;
+
+    }
+}
