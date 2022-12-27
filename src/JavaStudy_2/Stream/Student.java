047467ab0b699e4bb66dc204ea471f50b4cfbccb
Changhogod <younch8342@naver.com>
2022-12-28 02:03:05 +0900

    classify male & female from totalList

--- src/JavaStudy_2/Stream/Student.java
+++ src/JavaStudy_2/Stream/Student.java
@@ -1,12 +1,37 @@
 package JavaStudy_2.Stream;
 
 public class Student implements Comparable<Student>{
+    public enum Sex{MALE,FEMALE};
+    public enum City{SEOUL,PUSAN};
     private String name;
     private int score;
+    private Sex sex;
+    private City city;
 
     public Student(String name, int score) {
         this.name = name;
         this.score = score;
+    }
+
+    public Student(String name, int score, Sex sex) {
+        this.name = name;
+        this.score = score;
+        this.sex = sex;
+    }
+
+    public Student(String name, int score, Sex sex, City city) {
+        this.name = name;
+        this.score = score;
+        this.sex = sex;
+        this.city = city;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public City getCity() {
+        return city;
     }
 
     public String getName() {
---
+++ src/JavaStudy_2/Stream/ToListExample.java
@@ -0,0 +1,30 @@
+package JavaStudy_2.Stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ToListExample {
+    public static void main(String[] args) {
+        List<Student> totalList = Arrays.asList(new Student("윤창호",94,Student.Sex.MALE),
+                new Student("김경석",37,Student.Sex.MALE),
+        new Student("김진아",99,Student.Sex.FEMALE),
+        new Student("윤종호",98,Student.Sex.MALE),
+        new Student("장여진",100, Student.Sex.FEMALE));
+
+        System.out.println("남자 리스트");
+        List<Student> maleStudent = totalList.stream()
+                .filter(s->s.getSex()== Student.Sex.MALE)
+                .collect(Collectors.toList());
+        maleStudent.stream()
+                .forEach(s-> System.out.println(s.getName()) );
+
+        System.out.println();
+        System.out.println("여자 리스트");
+        List<Student> femaleList = totalList.stream()
+                .filter(s->s.getSex()== Student.Sex.FEMALE)
+                .collect(Collectors.toList());
+        femaleList.stream()
+                .forEach(s-> System.out.println(s.getName()));
+    }
+}
